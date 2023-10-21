bae5f07a89c1f5bc2f7b5aa52616a60b887536fc
Erbynn John <john.erbynn@gmail.com>
2023-10-21 06:30:21 +0000

    add integration test + update CI pipeline with integration test step

---
+++ test/StatlerWaldorfCorp.TeamService.Tests.Integration/IntegrationTest.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using Microsoft.AspNetCore.Mvc.Testing;
+//using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+//namespace StatlerWaldorfCorp.TeamService.Tests.Integration
+//{
+//    public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+//    {
+//        private readonly WebApplicationFactory<Program> _factory;
+//        private readonly HttpClient _client;
+
+//        private IntegrationTest(WebApplicationFactory<Program> factory)
+//        {
+//            _factory = factory;
+//            _client = _factory.CreateClient();
+//        }
+
+//        [Fact]
+//        public async Task TestGetEndpoint()
+//        {
+//            // Arrange (if necessary)
+
+//            // Act
+//            var response = await _client.GetAsync("/teams");
+
+
+//            // Assert
+//            response.EnsureSuccessStatusCode()
+//            //Assert.Equal("200", response.EnsureSuccessStatusCode().StatusCode)
+//            ; // Ensure a 2xx status code
+//        }
+
+//    }
+//}
+
---
+++ test/StatlerWaldorfCorp.TeamService.Tests.Integration/SimpleIntegrationTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using StatlerWaldorfCorp.TeamService.Models;
+
+namespace StatlerWaldorfCorp.TeamService.Tests.Integration;
+
+public class SimpleIntegrationTests
+{
+    //private readonly TestServer testServer;
+    private readonly WebApplicationFactory<Program> factory;
+    private readonly HttpClient testClient;
+
+    private readonly Team zombieTeamPayload;
+
+    public SimpleIntegrationTests()
+    {
+        factory = new WebApplicationFactory<Program>();
+        testClient = factory.CreateDefaultClient();
+
+        zombieTeamPayload = new Team()
+        {
+            ID = Guid.NewGuid(),
+            Name = "Zombie"
+        };
+    }
+
+    [Fact]
+    public async Task TestTeamPostAndGet()
+    {
+        var teamStringContent = new StringContent(JsonConvert.SerializeObject(zombieTeamPayload), UnicodeEncoding.UTF8, "application/json");
+
+        HttpResponseMessage postResponse = await testClient.PostAsync("/teams", teamStringContent);
+        postResponse.EnsureSuccessStatusCode();
+
+        var getResponse = await testClient.GetAsync("/teams");
+        getResponse.EnsureSuccessStatusCode();
+
+        string rawResponse = await getResponse.Content.ReadAsStringAsync();
+        List<Team> teams = JsonConvert.DeserializeObject<List<Team>>(rawResponse);
+
+        Assert.Single(teams);
+        Assert.Equal("Zombie", teams[0].Name);
+        Assert.Equal(zombieTeamPayload.ID, teams[0].ID);
+    }
+}
